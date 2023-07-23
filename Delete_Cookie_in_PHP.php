924a785c798a2241ef5919dc78ff22197082d596
Vijaya Amodkar <140308552+VijayaAmodkar@users.noreply.github.com>
2023-07-23 14:30:57 +0530

    Done

---
+++ Delete_Cookie_in_PHP.php
@@ -0,0 +1,11 @@
+<?php
+// set the expiration date to one hour ago
+setcookie("user", "", time() - 3600);
+?>
+<html>
+<body>
+<?php
+echo "Cookie 'user' is deleted.";
+?>
+</body>
+</html>
---
+++ Start a PHP session.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+// remove all session variables
+session_unset();
+// destroy the session
+session_destroy();
+?>
+</body>
+</html>
